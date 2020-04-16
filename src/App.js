1f59cd8abc6f7edecfa97d20e3ac9671f1b3ba41
TobiOladoja <tbfullstack@gmail.com>
2020-04-16 19:30:34 +0100

    Added searchbox and its styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import Search from './components/Search.js';
+
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
+      <header>
         <h1>Movie Database</h1>
       </header>
+      <main>
+        <Search />
+      </main>
     </div>
   );
 }
