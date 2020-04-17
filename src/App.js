960b22ad5f234e6a8458893c5a6c111412e725ef
TobiOladoja <tbfullstack@gmail.com>
2020-04-17 23:16:24 +0100

    Added results component to print out results, no design to it yet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import axios from 'axios';
 
 import Search from './components/Search.js';
+import Results from './components/Results.js';
 
 function App() {
+  const [state, setState] = useState({
+    s: '',
+    results: [],
+    selected: {},
+  });
+  const apiurl = 'http://www.omdbapi.com/?i=tt3896198&apikey=5f9eec05';
+
+  const search = (e) => {
+    if (e.key === 'Enter') {
+      axios(apiurl + '&s=' + state.s).then(({ data }) => {
+        let results = data.Search;
+
+        setState((prevState) => {
+          return { ...prevState, results: results };
+        });
+      });
+    }
+  };
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState((prevState) => {
+      return { ...prevState, s: s };
+    });
+  };
+
   return (
     <div className='App'>
       <header>
         <h1>Movie Database</h1>
       </header>
       <main>
-        <Search />
+        <Search handleInput={handleInput} search={search} />
+        <Results results={state.results} />
       </main>
     </div>
   );
